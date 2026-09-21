0772be7467970ffe8ad917db00f622af7bbdfe7c
timy575 <kim_engblom@hotmail.com>
2026-09-22 01:45:03 +0300

    B: done alot Player almost done

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
 import pygame
+
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0.0
     running = True
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x,y)
 
     while running:
         log_state()
 
+
         for event in pygame.event.get():
-            pass
+            if event.type == pygame.QUIT:
+                return
 
         screen.fill("black")
-
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 
